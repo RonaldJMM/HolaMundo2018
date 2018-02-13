9dfb2dd92444e4583dde151caceb00fb5cd90d95
Ronald Jefrey Moreno Mora <morenoronald7@gmail.com>
2018-02-13 09:30:18 -0500

    Primera Clase
    Clase con suma, resta, multiplicacion, division.

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main{
+	
+	public static void main(String[] arg){
+		System.out.println("");
+		System.out.println("Clase de programacion II");
+		System.out.println("");
+		System.out.println("\n\n\t\tEjercicio No.1");
+		System.out.println("");
+		System.out.println("");
+		
+		int var1 = 40;
+		int var2 = 20;
+		
+		System.out.println("La suma de la variable var1 + var2 ="+(var1+var2));
+		System.out.println("");
+		System.out.println("La resta de la variable var1 - var2 ="+(var1-var2));
+		System.out.println("");
+		System.out.println("La multiplicacion de la variable var1 * var2 ="+(var1*var2));
+		System.out.println("");
+		System.out.println("La division de la variable var1 / var2 ="+(var1/var2));
+		System.out.println("");
+		System.out.println("La variable var1 y var2 son:"+(var1)+" y "+(var2));
+		System.out.println("");
+	}
+
+}
